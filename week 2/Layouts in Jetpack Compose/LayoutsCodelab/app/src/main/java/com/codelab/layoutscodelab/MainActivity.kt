1c625db59e884c1efdd3983131222264c9f05e22
SonAuJiLi <zzilssun@gmail.com>
2021-11-13 19:27:44 +0900

    Init week 2

---
+++ week 2/Layouts in Jetpack Compose/LayoutsCodelab/app/src/main/java/com/codelab/layoutscodelab/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.codelab.layoutscodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.codelab.layoutscodelab.ui.theme.LayoutsCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LayoutsCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LayoutsCodelabTheme {
+        Greeting("Android")
+    }
+}
